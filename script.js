25658b1eeeeacbe4d0e77d9c2618320d77a7154e
Umesh Kumar Verma <umeshkumarhzb145@gmail.com>
2025-02-26 20:30:18 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const name ="rohit";
+const age =23;
+
+let balance = 420;
+
+function updateBlance (value){
+    balance+= value;
+}
+
+// crase website 
+// while(true){
+//     console.log(20);
+// }
+
+//update code ,or website crash ho gye: code ko revert back krna padega
+//Ownership of code
+// so use Tracker  : name, time
